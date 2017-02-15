95c82975a6ec11379d56b24988f9d9f0e495575a
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-02-15 19:41:02 +0000

    Added camera follow only z position

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	private Transform player;
+	private float offset = 5f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 cameraPos = transform.position;
+		cameraPos.z = player.position.z - offset;
+		transform.position = cameraPos;
+	}
+}
