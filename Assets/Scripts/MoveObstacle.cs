0afee7ce401034c9a092d9fb1603456f79cccd4d
Jorge <up201305617@fe.up.pt>
2017-02-22 22:47:00 +0000

    obstacles spawning and moving

---
+++ Assets/Scripts/MoveObstacle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacle : MonoBehaviour
+{
+    private Vector3 movement;
+    private Rigidbody rigidbodyComponent;
+    public Vector3 speed;
+    public Vector3 direction;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (transform.position.z < -20)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            movement = new Vector3(speed.x * direction.x, speed.y * direction.y, speed.z * direction.z);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (rigidbodyComponent == null)
+        {
+            rigidbodyComponent = GetComponent<Rigidbody>();
+        }
+
+        rigidbodyComponent.velocity = movement;
+    }
+}
---
+++ Assets/Scripts/SpawnObstacle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacle : MonoBehaviour
+{
+    private int[] tracks = {-3,0,3};
+    public float spawningTime = 10.0f;
+    public GameObject obstaclePrefab;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        CreateObstacle();
+    }
+
+    void CreateObstacle()
+    {
+        spawningTime -= Time.deltaTime;
+
+        if (spawningTime <= 0)
+        {
+            Instantiate(obstaclePrefab, new Vector3(tracks[Random.Range(0,2)],0, 35), Quaternion.identity);
+            spawningTime = 5.0f;
+        }
+    }
+}
