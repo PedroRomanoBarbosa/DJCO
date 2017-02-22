329560f80b925ae64b67defd123695febce00e1e
Jorge <up201305617@fe.up.pt>
2017-02-22 14:37:56 +0000

    infinity ground

---
+++ Assets/Scripts/InfinityGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfinityGround : MonoBehaviour
+{
+    private float GroundLength = 50f;
+    private float GroundSpeed = 5f;
+    public GameObject Ground;
+
+    void Update()
+    {
+        Vector3 GroundPos = Ground.transform.position;
+        GroundPos.z -= GroundSpeed * Time.deltaTime;
+
+        if (GroundPos.z < -GroundLength / 2)
+        {
+            GroundPos.z += GroundLength;
+        }
+
+        Ground.transform.position = GroundPos;
+    }
+}
