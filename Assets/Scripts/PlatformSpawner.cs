b8527274331a867ee889804cdcceabc10ac42137
Tropical <up201306506@gcloud.fe.up.pt>
2017-03-06 18:24:55 +0000

    Infinite Empty Corridor Sections Spawning, Script ready for corridor prefabs

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -8,28 +8,44 @@ public class PlatformSpawner : MonoBehaviour
     public GameObject platformPrefab;
     public GameObject emptyCorridorPrefab;
 
+    private GameObject latestCorridor;
+
     private GameGlobals game;
-    private float spawnPoint = 145f;
 
     // Use this for initialization
     void Start()
     {
         game = GameObject.Find("GameController").GetComponent<GameGlobals>();
-        CreateEmptySection(45f);
-        CreateEmptySection(95f);
+        CreateSection_EmptySection(45f);
+        latestCorridor = CreateSection_EmptySection(95f);
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        if(latestCorridor.transform.position.z < 95)
+        {
+            latestCorridor = CreateRandomSection(latestCorridor.transform.position.z + 50);
+        }
     }
 
-    void CreateEmptySection(float position)
+
+    GameObject CreateRandomSection(float position)
+    {
+        //Mudar isto para um random entre os outros tipos de secções
+        GameObject newSection = CreateSection_EmptySection(position);
+
+
+        return newSection;
+    }
+
+
+    GameObject CreateSection_EmptySection(float position)
     {
         GameObject newEmptyCorridor = Instantiate(emptyCorridorPrefab, new Vector3(0,-0.05f,position), Quaternion.identity);
         newEmptyCorridor.transform.parent = GameObject.Find("Platforms").transform;
-
+        return newEmptyCorridor;
     }
 
 }
