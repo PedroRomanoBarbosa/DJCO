337644b3a5ec0ada288b3c24b5cffd5b89a80235
Tropical <up201306506@gcloud.fe.up.pt>
2017-03-06 22:22:34 +0000

    Character has continuous sideways movement and proper animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-	private CharacterController controller;
 	private float verticalVelocity = 0.0f;
 	private float timer = 0.0f;
 	private bool jumping = false;
@@ -13,29 +12,43 @@ public class PlayerMovement : MonoBehaviour {
 	public float jumpSpeed = 10f;
 	public float gravity = 9.8f;
 
-	void Start () {
-		controller = GetComponent<CharacterController> ();
-	}
+    private CharacterController controller;
+    private Animator animator;
+    private GameGlobals game;
+
+    void Start () {
+        game = GameObject.Find("GameController").GetComponent<GameGlobals>();
+        controller = GetComponent<CharacterController> ();
+        animator = GetComponent<Animator>();
+        animator.SetFloat("MoveSpeed", game.speed);
+    }
 
 	void Update () {
-		if (controller.isGrounded) {
+
+        animator.SetFloat("MoveSpeed", game.speed);
+
+        if (controller.isGrounded) {
 			verticalVelocity = 0.0f;
 			timer = 0.0f;
 			jumping = false;
+            if(!animator.GetBool("Grounded"))
+                animator.SetBool("Grounded", true);
 		} else {
 			verticalVelocity += -gravity * Time.deltaTime;
 		}
 		moveVector = Vector3.zero;
 
 		// STRAFE
-		moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
 
 		// JUMP
 		if(Input.GetKeyDown("space")) {
 			jumping = true;
 		}
 		if (jumping) {
-			timer += Time.deltaTime;
+            if (animator.GetBool("Grounded"))
+                animator.SetBool("Grounded", false);
+            timer += Time.deltaTime;
 			if (timer <= 0.1f) {
 				if (Input.GetKey("space")) {
 					verticalVelocity += jumpFunction (timer / 0.25f) * Time.deltaTime * jumpSpeed;
