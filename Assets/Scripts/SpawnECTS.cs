64951eac928015ea17a447381a962193d84055d4
Tropical <up201306506@gcloud.fe.up.pt>
2017-03-07 12:40:13 +0000

    minor change

--- Assets/Scripts/SpawnECTS.cs
+++ Assets/Scripts/SpawnECTS.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnECTS : MonoBehaviour
 {
-    public float spawningTime = 10.0f;
+    public float spawningTime = 5.0f;
     public GameObject ECTPrefab;
     private GameGlobals game;
 
@@ -28,7 +28,7 @@ public class SpawnECTS : MonoBehaviour
 
     void CreateObstacle()
     {  
-        GameObject newObstacle = Instantiate(ECTPrefab, new Vector3(Random.Range(-5f, 5f), 1.5f, 35), ECTPrefab.transform.rotation);
+        GameObject newObstacle = Instantiate(ECTPrefab, new Vector3(Random.Range(-5f, 5f), 1.5f, 100), ECTPrefab.transform.rotation);
         newObstacle.transform.parent = GameObject.Find("Obstacles").transform;
         spawningTime = 5.0f;
     }
