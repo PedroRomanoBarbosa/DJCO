0a1dd296cebddfc31145c197046337809a618132
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-03-09 22:59:49 +0000

    Added coin rotation

---
+++ Assets/Scripts/ContinuousRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuousRotation : MonoBehaviour {
+	private float rotation;
+	public int speed;
+
+	void Update () {
+		transform.Rotate(Vector3.up * speed * Time.deltaTime, Space.Self);
+	}
+}
