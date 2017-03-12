2004361884662503e81c6b25618aeb4708333c11
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2017-03-12 19:28:52 +0000

    Added coin generation

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -8,7 +8,9 @@ public class Generate : MonoBehaviour {
 		Player,
 		Column,
 		Bench,
-		Door
+		Door,
+		Coin,
+		BenchCoin
 	}
 	private enum Moves {
 		Up,
@@ -22,8 +24,8 @@ public class Generate : MonoBehaviour {
 		Hard
 	}
 	private int numPlayerSpaces;
-	private List<KeyValuePair<int,int>> columnValidPositions, benchValidPositions, doorValidPositions;
-	private int difficulty, numOfObstacles, numCoins;
+	private List<KeyValuePair<int,int>> columnValidPositions, benchValidPositions, doorValidPositions, coinValidPositions;
+	private int difficulty, numObstacles, numCoins;
 	private int playerY;
 	private Types[,] matrix;
 	public int Columns, Lines;
@@ -60,11 +62,16 @@ public class Generate : MonoBehaviour {
 				PlayerX += 1;
 				break;
 			}
+
 			AssignPosition ();
 		}
 
 		setDifficulty ();
+
 		assignObstacles ();
+
+		assignCoins ();
+
 		instantiateObstacles ();
 
 	}
@@ -102,15 +109,43 @@ public class Generate : MonoBehaviour {
 		columnValidPositions = new List<KeyValuePair<int, int>> ();
 		benchValidPositions = new List<KeyValuePair<int, int>> ();
 		doorValidPositions = new List<KeyValuePair<int, int>> ();
+
+		// Iterate the position matrix
 		for (int y = 0; y < Columns; y++) {
 			for (int x = 0; x < Lines; x++) {
+				KeyValuePair<int, int> position = new KeyValuePair<int,int> (x, y);
+
+				// Benches can be put in player positions only if there are player positions adjacent at least in one direction
+				// For first and last columns
+				if (x == 0 || x == Lines - 1) {
+					if (y != 0 && y != Columns - 1) {
+						if (matrix [y - 1, x] == Types.Player && matrix [y + 1, x] == Types.Player) {
+							benchValidPositions.Add (position);
+						}
+					}
+				// For first and last rows
+				} else if (y == 0 || y == Columns - 1) {
+					if (x != 0 && x != Lines - 1) {
+						if (matrix [y, x - 1] == Types.Player && matrix [y, x + 1] == Types.Player) {
+							benchValidPositions.Add (position);
+						}
+					}
+				// For the rest of the positions that are not on the edges of the matrix
+				} else {
+					if ( (matrix [y, x - 1] == Types.Player && matrix [y, x + 1] == Types.Player) || (matrix [y - 1, x] == Types.Player && matrix [y + 1, x] == Types.Player) ) {
+						benchValidPositions.Add (position);
+					}
+				}
+
+				// Empty spaces
 				if (matrix [y, x] == Types.Empty) {
-					KeyValuePair<int, int> position = new KeyValuePair<int,int> (x, y);
 					benchValidPositions.Add (position);
+
 					// Left side columns
 					if (x == 0) {
 						columnValidPositions.Add (position);
 					}
+
 					// Door and right side columns
 					if (x == Lines - 1) {
 						columnValidPositions.Add (position);
@@ -121,30 +156,78 @@ public class Generate : MonoBehaviour {
 		}
 	}
 
-	private void setDifficulty() {
-		difficulty = Random.Range ((int)Difficulties.Easy, (int)Difficulties.Hard + 1);
-		switch (difficulty) {
-		case (int) Difficulties.NoBrainer:
-			numOfObstacles = Random.Range (0, 1 + 1);
-			break;
-		case (int) Difficulties.Easy:
-			numOfObstacles = Random.Range (1, 2 + 1);
-			numCoins = Random.Range (0, 1 + 1);
-			break;
-		case (int) Difficulties.Medium:
-			numOfObstacles = Random.Range (2, 3 + 1);
-			numCoins = 1;
-			break;
-		case (int) Difficulties.Hard:
-			numOfObstacles = Random.Range (3, 4 + 1);
-			numCoins = Random.Range (1, 2 + 1);
-			break;
+	private void getValidCoinPositions () {
+		coinValidPositions = new List<KeyValuePair<int, int>> ();
+		for(int y = 0; y < Columns - 1; y++) {
+			for(int x = 0; x < Lines - 1; x++) {
+				if (matrix [y, x] == Types.Empty || matrix [y, x] == Types.Bench || matrix [y, x] == Types.Player) {
+					coinValidPositions.Add (new KeyValuePair<int, int>(x, y));
+				}
+			}
 		}
 	}
 
+	private void setDifficulty() {
+		difficulty = Random.Range ((int)Difficulties.NoBrainer, (int)Difficulties.Hard + 1);
+		switch (difficulty) {
+		case (int) Difficulties.NoBrainer:
+			numObstacles = Random.Range (0, 1 + 1);
+			numCoins = 0;
+			break;
+		case (int) Difficulties.Easy:
+			numObstacles = Random.Range (1, 2 + 1);
+			numCoins = 1;
+			break;
+		case (int) Difficulties.Medium:
+			numObstacles = Random.Range (2, 3 + 1);
+			numCoins = numCoins = Random.Range (1, 2 + 1);
+			break;
+		case (int) Difficulties.Hard:
+			numObstacles = Random.Range (3, 4 + 1);
+			numCoins = Random.Range (2, 3 + 1);
+			break;
+		}
+		Debug.Log (numCoins);
+	}
+
+	/**
+	 * TODO: Put obstacles in the right position
+	 */
+	private void instantiateObstacles () {
+		for (int y = 0; y < Columns; y++) {
+			for (int x = 0; x < Lines; x++) {
+				Types type = matrix [y, x];
+				switch(type){
+				case Types.Column:
+					Instantiate (Column, new Vector3 (5 * x - 5, 1, 5 * y), Quaternion.identity);
+					break;
+				case Types.Bench:
+					Instantiate (Bench, new Vector3 (5 * x - 5, 2, 5 * y), Quaternion.identity);
+					break;
+				case Types.Door:
+					Instantiate (Door, new Vector3 (5 * x - 5, 4, 5 * y), Quaternion.identity);
+					break;
+				case Types.Coin:
+					Instantiate (Coin, new Vector3 (5 * x - 5, 2, 5 * y), Quaternion.identity);
+					break;
+				case Types.BenchCoin:
+					Instantiate (Bench, new Vector3 (5 * x - 5, 2, 5 * y), Quaternion.identity);
+					Instantiate (Coin, new Vector3 (5 * x - 5, 4, 5 * y), Quaternion.identity);
+					break;
+				}
+			}
+		}
+	}
+
+	private void AssignPosition () {
+		matrix [playerY, PlayerX] = Types.Player;
+		numPlayerSpaces++;
+	}
+
+	/* TODO check rndom algorithm(it's causing errors) */
 	private void assignObstacles() {
 		int counter = 0;
-		while (counter < numOfObstacles) {
+		while (counter < numObstacles) {
 			getValidObstaclePositions ();
 			int totalValid = columnValidPositions.Count + benchValidPositions.Count + doorValidPositions.Count;
 			int rand = Random.Range(0, totalValid);
@@ -163,33 +246,21 @@ public class Generate : MonoBehaviour {
 		}
 	}
 
-	/**
-	 * TODO: Put obstacles in the right position
-	 */
-	private void instantiateObstacles () {
-		for (int y = 0; y < Columns; y++) {
-			for (int x = 0; x < Lines; x++) {
-				Types type = matrix [y, x];
-				switch(type){
-				case Types.Column:
-					Instantiate (Column, new Vector3 (5 * x - 5, 1, 5 * y), Quaternion.identity);
-					break;
-				case Types.Bench:
-					Debug.Log ("bench");
-					Instantiate (Bench, new Vector3 (5 * x - 5, 2, 5 * y), Quaternion.identity);
-					break;
-				case Types.Door:
-					Instantiate (Door, new Vector3 (5 * x - 5, 4, 5 * y), Quaternion.identity);
-					break;
-				}
+	private void assignCoins() {
+		getValidCoinPositions ();
+		int counter = 0;
+		while (counter < numCoins && coinValidPositions.Count > 0) {
+			int total = coinValidPositions.Count;
+			int rand = Random.Range (0, total);
+			KeyValuePair<int, int> position = coinValidPositions[rand];
+			if (matrix [position.Value, position.Key] == Types.Bench) {
+				matrix [position.Value, position.Key] = Types.BenchCoin;
+			} else {
+				matrix [position.Value, position.Key] = Types.Coin;
 			}
+			coinValidPositions.RemoveAt (rand);
+			counter++;
 		}
-	}
-
-	private void AssignPosition () {
-		matrix [playerY, PlayerX] = Types.Player;
-		Instantiate (Player, new Vector3 (5 * PlayerX - 5, 0, 5 * playerY), Quaternion.identity);
-		numPlayerSpaces++;
 	}
 
 }
